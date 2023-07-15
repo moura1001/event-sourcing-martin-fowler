4a83aa0afdd2e337b8c7a17546a01dd5a10b2d81
moura1001 <gmouraneto01@gmail.com>
2023-07-15 11:43:01 -0300

    initial setup

---
+++ src/test/java/moura/learn/eventsourcing/EventSourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package moura.learn.eventsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSourcingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
